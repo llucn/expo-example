e429e82360f4fbdd8da99bb881f749591b877def
Lane Lu <161492667+llucn@users.noreply.github.com>
2025-04-24 23:30:52 +0800

    read ndef tag

--- app/nfc/nfcIntegration.tsx
+++ app/nfc/nfcIntegration.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
 
 import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function NFCIntegrationScreen() {
-  const [ value, setValue ] = useState('N/A');
+  const [ value, setValue ] = useState<string>('N/A');
+  const [ payload, setPayload ] = useState<number[]>([]);
   
   useEffect(() => {
     // Initialize NFC Manager
@@ -13,7 +15,10 @@ export default function NFCIntegrationScreen() {
     // Add event listeners
     NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag: any) => {
       console.log('Tag Discovered', tag);
-      setValue(tag);
+      setValue(JSON.stringify(tag));
+      if (tag.ndefMessage && tag.ndefMessage[0]) {
+        setPayload(tag.ndefMessage[0].payload);
+      }
       NfcManager.setAlertMessageIOS('NFC Tag Discovered');
       NfcManager.unregisterTagEvent().catch(() => 0);
     });
@@ -30,6 +35,9 @@ export default function NFCIntegrationScreen() {
   }, []);
 
   return (
-    <ThemedText>NFC Tag: {value}</ThemedText>
+    <ThemedView>
+      <ThemedText>NFC Tag: {JSON.stringify(value)}</ThemedText>
+      <ThemedText>Payload: {String.fromCharCode(...payload)}</ThemedText>
+    </ThemedView>
   );
 }
